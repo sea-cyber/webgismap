c0a8418354dd61fa31d38a11a298ef6801926156
Admin <12564610+sea-cyber_0@user.noreply.gitee.com>
2023-03-18 22:52:10 +0800

    第二次尝试

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+   <div class="hell"><HelloWorld msg="Welcome to Your Vue.js App"/></div>
+    
+    <div class="button"><ButtonCounter/></div> 
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import ButtonCounter from './components/ButtonCounter.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    ButtonCounter
+  }
+}
+</script>
+
+<style>
+#app {
+  width: 400px;
+  height: 300px;
+  border: 1px solid black;
+  margin: auto;
+}
+.hell,.button{
+  width: 400px;
+  height: 150px;
+  border: 1px  solid red;
+  margin: auto;
+}
+</style>
---
+++ src/components/ButtonCounter.vue
@@ -0,0 +1,21 @@
+<template>
+    <button @click="count++" class="">
+      You clicked me {{ count }} times.
+    </button>
+  </template>
+<script>
+export default {
+    name:'ButtonCounter',
+    data(){
+        return{
+            count:0
+        }
+    }
+}
+</script>
+<style>
+button
+   { width: 100px;
+    height: 100px;}
+
+</style>
